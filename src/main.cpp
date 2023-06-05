f50c9ec37335f5995ab140208a38276b6992da20
Richard Diaz <richard.sd0007@gmail.com>
2023-06-05 00:10:00 -0400

    add main

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "lexer.hpp"
+
+int main() {
+  const std::string input = R"(
+    let five = 5;
+    const ten = 10;
+
+    const add = function(x, y) {
+        x + y;
+    };
+
+    let res = add(x, y);
+    )";
+
+  constexpr TokenType testTypes[]{
+      TokenType::Let,        TokenType::Identifier, TokenType::Assign,
+      TokenType::Number,     TokenType::Semicolon,  TokenType::Const,
+      TokenType::Identifier, TokenType::Assign,     TokenType::Number,
+      TokenType::Semicolon,  TokenType::Const,      TokenType::Identifier,
+      TokenType::Assign,     TokenType::Function,   TokenType::LeftParen,
+      TokenType::Identifier, TokenType::Comma,      TokenType::Identifier,
+      TokenType::RightParen, TokenType::LeftBrace,  TokenType::Identifier,
+      TokenType::Plus,       TokenType::Identifier, TokenType::Semicolon,
+      TokenType::RightBrace, TokenType::Semicolon,  TokenType::Let,
+      TokenType::Identifier, TokenType::Assign,     TokenType::Identifier,
+      TokenType::LeftParen,  TokenType::Identifier, TokenType::Comma,
+      TokenType::Identifier, TokenType::RightParen, TokenType::Semicolon};
+
+  Lexer l{input};
+
+  for (const auto type : testTypes) {
+    const auto token = l.peek();
+
+    std::cout << "type : " << std::to_string(type) 
+            << " token.type : " << std::to_string(token.type) << std::endl;
+    std::cout << std::boolalpha << "Equal : " << (type == token.type);
+  }
+
+  return 0;
+}
